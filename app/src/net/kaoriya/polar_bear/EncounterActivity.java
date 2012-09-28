02a9956abb05489236af43b871d1ac270383a37e
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-09-28 23:12:49 +0900

    first import

---
+++ app/src/net/kaoriya/polar_bear/EncounterActivity.java
@@ -0,0 +1,15 @@
+package net.kaoriya.polar_bear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EncounterActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.encounter);
+    }
+}
