c84eade9e3e6f79ee0f3d01d01337ee82f4ad025
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-09-29 02:41:01 +0900

    implement random animation

--- app/src/net/kaoriya/polar_bear/EncounterActivity.java
+++ app/src/net/kaoriya/polar_bear/EncounterActivity.java
@@ -1,10 +1,15 @@
 package net.kaoriya.polar_bear;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.graphics.Matrix;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -12,13 +17,41 @@ public class EncounterActivity extends Activity
     implements PolarBear
 {
 
-    private static int[] BEAR_IMAGES = new int[] {
+    public final static long ANIMATION_DELAY = 0;
+
+    public final static long ANIMATION_PERIOD = 600;
+
+    private final static int[] BEAR_IMAGES = new int[] {
         R.drawable.bear01a,
         R.drawable.bear02a,
         R.drawable.bear03a,
         R.drawable.bear04a,
         R.drawable.bear05a,
     };
+
+    class AnimationTask extends TimerTask
+    {
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                public void run() {
+                    updateBear();
+                }
+            });
+        }
+    }
+
+    private boolean bearActive = false;
+
+    private boolean bearFocus = false;
+
+    private Handler handler = new Handler();
+
+    private Timer animationTimer = null;
+
+    private int animationFrame = 0;
+
+    private Random random = new Random();
 
     /** Called when the activity is first created. */
     @Override
@@ -32,15 +65,21 @@ public class EncounterActivity extends Activity
     public void onResume()
     {
         super.onResume();
+        setBearActive(true);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        setBearActive(false);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus)
     {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            setBearImage(R.drawable.bear01a, false);
-        }
+        setBearFocus(hasFocus);
     }
 
     private void setBearImage(int id, boolean reverse)
@@ -65,4 +104,44 @@ public class EncounterActivity extends Activity
         v.setImageMatrix(m);
         v.setImageDrawable(d);
     }
+
+    private void setBearActive(boolean value)
+    {
+        this.bearActive = value;
+        checkBearAnimation();
+    }
+
+    private void setBearFocus(boolean value)
+    {
+        this.bearFocus = value;
+        checkBearAnimation();
+    }
+
+    private synchronized void checkBearAnimation()
+    {
+        if (this.bearActive && this.bearFocus) {
+            // start animation.
+            if (this.animationTimer != null) {
+                this.animationTimer.cancel();
+            }
+            this.animationTimer = new Timer(false);
+            this.animationTimer.schedule(new AnimationTask(),
+                    ANIMATION_DELAY, ANIMATION_PERIOD);
+            setBearImage(R.drawable.bear01a, false);
+        } else {
+            // stop animation.
+            if (this.animationTimer != null) {
+                this.animationTimer.cancel();
+                this.animationTimer = null;
+            }
+        }
+    }
+
+    private void updateBear()
+    {
+        int index = this.random.nextInt(BEAR_IMAGES.length);
+        boolean reverse = this.random.nextBoolean();
+        setBearImage(BEAR_IMAGES[index], reverse);
+    }
+
 }
